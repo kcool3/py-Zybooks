fe234d41ee37e2de432e0dce005c2ee01f506ad4
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-11-23 15:11:36 -0700

    Add files via upload

---
+++ Q12.py
@@ -0,0 +1,14 @@
+"""Write a program whose input is two integers, and whose output is the first integer and
+subsequent increments of 5 as long as the value is less than or equal to the second integer.
+Ex. If the input is: -15 10 The output is: -15 -10 -5 0 5 10
+Ex. If the second integer is less than the first. The output is: Second integer can't be less than the first.
+"""
+
+start = int(input())
+end = int(input())
+if start > end:
+    print("Second integer can't be less than the first.")
+else:
+    while start <= end:
+        print(start, end=' ')
+        start += 5
