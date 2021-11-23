199306397188a2a5092a738f1ced77362903e770
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-11-23 12:21:32 -0700

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -5,10 +5,10 @@ print(f'Amount: ${dollars:.2f}') Ex. If the input is: 4 3 2 1
 where 4 is the number of quarters, 3 is the number of dimes, 2 is the number of nickels,
 and 1 is the number of pennies, the output is: Amount:$1.41. For simplicity, assume the input is a non-negative number.
 """
-quarters = int(input(4))
-dimes = int(input(3))
-nickels = int(input(2))
-pennies = int(input(1))
+quarters = int(input())
+dimes = int(input())
+nickels = int(input())
+pennies = int(input())
 
 Amount = quarters*0.25 + dimes*0.10 + nickels*0.05 + pennies*0.01
 #Prints out Amount within two decimal places.
