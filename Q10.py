995e332b91708a96437e0a20796b37073ea2cbf6
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-11-23 14:42:46 -0700

    Add files via upload

---
+++ Q10.py
@@ -0,0 +1,5 @@
+"""Write a program that prints a formatted "No parking" sign as shown below.
+Note: the first line has two leading spaces."""
+#this one is easy! remember the two leading spaces and make sure things align.
+print('   NO PARKING')
+print('2:00 - 6:00 a.m.')
