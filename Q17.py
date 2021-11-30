3aeb31d547c9b742d9175c7eb383613510a9b636
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-11-30 08:53:34 -0700

    Add files via upload

---
+++ Q17.py
@@ -0,0 +1,13 @@
+'''Complete the calc_average() method that has an integer list parameter and returns the average value of the elements in the array as a double.
+Ex. If input array is:
+1 2 3 4 5
+then the returned average will be:
+3.0'''
+
+def calc_average(nums):
+    return sum(nums) / len(nums)
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4, 5]
+    print(calc_average(nums))
+
+    # this should return 3.0.
