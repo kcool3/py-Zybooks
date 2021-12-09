dae01b7dbdfca33161651006859f0a0001ecdd0d
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-12-09 12:20:45 -0700

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -6,19 +6,19 @@ If the input is: 45 the output is: 1 Quarter 2 Dimes"""
 
 def main():
     input_val = int(input())
+    Dollars = input_val // 100
+    input_val %= 100
     if input_val <= 0:
-        print('No change')
-    else:
-        Dollars = input_val // 100
-        input_val %= 100
-        Quarters = input_val // 25
-        input_val %= 25
-        Dimes = input_val //10
-        input_val %= 10
-        Nickels = input_val // 5
-        input_val %= 5
-        Pennies = input_val
-        
+         print('No change')
+    Quarters = input_val // 25
+    input_val %= 25
+    Dimes = input_val //10
+    input_val %= 10
+    Nickels = input_val // 5
+    input_val %= 5
+    Pennies = input_val
+    
+    
     if Dollars > 1:
         print(' %d Dollars' % Dollars)
     elif Dollars == 1:
@@ -40,7 +40,6 @@ def main():
         print(' %d Pennies' % Pennies)
     elif Pennies == 1:
         print(' %d Penny' % Pennies)
+   
 main()
-
-        
 
