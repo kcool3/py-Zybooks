64dbf9b31787908341684e135b658ec82417a0ed
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-12-21 13:36:18 -0700

    Update Q7.py

--- Q7.py
+++ Q7.py
@@ -3,14 +3,12 @@ Examples are: "bob", "sees", or "never odd or even"(ignoring spaces). Write a pr
 outputs whether the input is a palindrome. Ex. If the input is: bob
 The output is: bob is a palindrome."""
 
-word = input()
-reverseString = ""
-for i in range(len(word)):
-    if(word[i]!= ' '):
-        reverseString = word[i] + reverseString
-if (word == reverseString):
-    print(word, "is a palindrome")
+user_input = input()
+normal_str = user_input.replace(" ", "")
+reverse_str = normal_str[::-1]
+if normal_str == reverse_str:
+    print('{} is a palindrome'.format(user_input))
 else:
-    print(word, "is not a palindrome")
+    print('{} is not a palindrome'.format(user_input))
     
     
